6a22e59d61e28c11b37314a60be0e3be8fa409d7
James Corcoran <jpcsoccer1234@gmail.com>
2021-04-14 17:56:15 -0400

    rearranged files and folders, added some code to start with on js file

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,42 @@
 const generateMarkdown = require("../utils/generateMarkdown");
 const inquirer = require("inquirer");
-
+// write (fs)
 
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        // Use user feedback for... whatever!!
+      })
+}
 
 // Function call to initialize app
 init();
 
-generateMarkdown();
+//generateMarkdown();
