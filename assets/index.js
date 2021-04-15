e86617e98f9821d1a442f2f3f7fa838060a0b2f2
James Corcoran <jpcsoccer1234@gmail.com>
2021-04-15 15:44:10 -0400

    added list of questions user will be asked

--- assets/index.js
+++ assets/index.js
@@ -7,21 +7,58 @@ const util = require('util');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the project title?',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+        type: 'input',
+        name: 'description',
+        message: 'Write a brief description of your project: ',
     },
     {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+        type: 'input',
+        name: 'installation',
+        message: 'Provide any installation instructions',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What will this project be used for',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who are the contributors of this project?',
+        //check to see if this is correct
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide any test instructions',
+        //check this one too
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the appropriate license for this project',
+        choices: [  "Apache","Academic","GNU","ISC","MIT","Mozilla","Open"],
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Please enter your Github username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address: ',
+    },
+    {
+        //not sure about this one: table of contents
+        type: 'input',
+        name: 'tableOfContents',
+        message:'message',
     },
   ];
 
@@ -39,4 +76,4 @@ function init() {
 // Function call to initialize app
 init();
 
-//generateMarkdown();
+generateMarkdown();
